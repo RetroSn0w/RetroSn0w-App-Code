7a2ed05ec2784a8a844a41f9f4c7edde800f3a0e
RetroSn0w <86943527+RetroSn0w@users.noreply.github.com>
2021-07-20 22:49:39 -0400

    Add files via upload

---
+++ Pluvia/autoinstall435.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ~/Documents/RetroSn0w/CFJB_v1.6.0_1F79
+rm -rf shsh/
+mkdir shsh
+./idevicerestore -t iPhone3,1_7.1.2_11D257_Restore.ipsw
+model="iPhone3,1"
+ecid=`ioreg -l -w0 | grep "USB Serial Number" | grep -m 1 "iBoot-574.4" | sed 's/^.*ECID://' | sed 's/ .*//'`
+cd shsh/
+ecidf="$(printf "%d" 0x$ecid)-${model}-4.3.5.shsh"
+mv *.shsh $ecidf
+cd ..
+./iPwnder32 -p
+./idevicerestore -e -w iPhone3,1_4.3.5_8L1_Custom.ipsw
+mv ~/Documents/RetroSn0w/CFJB_v1.6.0_1F79/iPhone3,1_4.3.5_8L1_Restore.ipsw ~/Downloads
+mv ~/Documents/RetroSn0w/CFJB_v1.6.0_1F79/iPhone3,1_7.1.2_11D257_Restore.ipsw ~/Downloads
+
