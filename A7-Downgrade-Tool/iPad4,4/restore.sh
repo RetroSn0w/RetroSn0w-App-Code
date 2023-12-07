4e135f75c4353d2a3101270eba40784235200cef
RetroSn0w <86943527+RetroSn0w@users.noreply.github.com>
2023-12-07 00:11:15 -0500

    Update restore.sh

--- A7-Downgrade-Tool/iPad4,4/restore.sh
+++ A7-Downgrade-Tool/iPad4,4/restore.sh
@@ -34,9 +34,9 @@ ecid=$(./tools/igetnonce | grep "ECID=.*" -o | cut -c 6-)
 echo "$ecid"
 ./tools/tsschecker -m iPad4,4/iPad4,4.plist -e $ecid -d iPad4,4 -s -B j85ap --apnonce $nonce
 mv *.shsh blob.shsh2
-./tools/irecovery -f tools/blankfile.txt
-./tools/irecovery -f iPad4,4/temp/iBSS.ipad4b.RELEASE.im4p
-./tools/irecovery -f iPad4,4/temp/iBEC.ipad4b.RELEASE.im4p
+./tools/dependencies/libirecovery/tools/irecovery -f tools/blankfile.txt
+./tools/dependencies/libirecovery/tools/irecovery -f iPad4,4/temp/iBSS.ipad4b.RELEASE.im4p
+./tools/dependencies/libirecovery/tools/irecovery -f iPad4,4/temp/iBEC.ipad4b.RELEASE.im4p
 sleep 5
 ./tools/futurerestore -t blob.shsh2 --no-baseband -p iPad4,4/iPad4,4.plist -s iPad4,4/temp/sep.im4p -m iPad4,4/iPad4,4.plist iPad4,4/work
 cd iPad4,4
